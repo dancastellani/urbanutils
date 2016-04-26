6fdff27f4741e10acea518f6cb6ac91af1635bc5
Daniel Castellani <daniel.castellani@nyu.edu>
2016-04-26 17:50:57 -0400

    Changed name of project

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-    name = "urban_utils",
+    name = "urbanutils",
     version = "0.1.0",
     author = "Daniel Castellani",
     author_email = "daniel.castellani@nyu.edu",
